59aee44327fa5b65d41e5d29a8781b261bf35bb6
enextus <enextus@gmail.com>
2020-11-24 18:30:07 +0100

    upd

--- src/lesson01/hw/GcdSolverTest.java
+++ src/lesson01/hw/GcdSolverTest.java
@@ -10,11 +10,21 @@ class GcdSolverTest {
 
     @Test
     public void testGcd_nEquals15_20_5() {
-        assertEquals(5, gcdsolver.getGcd(15,20));
+        assertEquals(5, gcdsolver.getGcd(15, 20));
     }
 
     @Test
-    public void test_nEquals1071_462_21(){
+    public void testGcd_nEquals20_15_5() {
+        assertEquals(5, gcdsolver.getGcd(20, 15));
+    }
+
+    @Test
+    public void test_nEquals1071_462_21() {
         assertEquals(21, gcdsolver.getGcd(1071, 462));
+    }
+
+    @Test
+    public void test_nEquals462_1071_21() {
+        assertEquals(21, gcdsolver.getGcd(462, 1071));
     }
 }
