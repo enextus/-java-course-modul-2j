36a289b000071e71e075de27fe11bd4f8a8e8e5f
enextus <enextus@gmail.com>
2020-11-24 17:35:55 +0100

    .gitignore

--- src/lesson01/sqr_recursive/SqrSolverTest.java
+++ src/lesson01/sqr_recursive/SqrSolverTest.java
@@ -1,4 +1,23 @@
 package lesson01.sqr_recursive;
 
-public class SqrSolverTest {
+import org.junit.jupiter.api.Test;
+
+class SqrSolverTest {
+
+    SqrSolver sqrSolver = new SqrSolver();
+
+    @Test
+    public void testSqr_nEquals1_1() {
+        assertEquals(1, sqrSolver.sqr(1));
+    }
+
+    @Test
+    public void testSqr_nEquals10_100() {
+        assertEquals(100, sqrSolver.sqr(10));
+    }
+
+    @Test
+    public void testSqr_nEquals13_169() {
+        assertEquals(169, sqrSolver.sqr(13));
+    }
 }
