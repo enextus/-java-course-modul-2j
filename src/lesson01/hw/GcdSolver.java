245ccef812b3d8ecf4eac019fb497003d139a94b
enextus <enextus@gmail.com>
2020-11-24 18:17:24 +0100

    test upd

--- src/lesson01/hw/GcdSolver.java
+++ src/lesson01/hw/GcdSolver.java
@@ -5,6 +5,13 @@ package lesson01.hw;
  */
 public class GcdSolver {
 
+    /**
+     * The method accepts only positive int numbers.
+     *
+     * @param a int number
+     * @param b int number
+     * @return GCD of a and b
+     */
     public int getGcd(int a, int b) {
         return b == 0 ? a : getGcd(b, a % b);
     }
